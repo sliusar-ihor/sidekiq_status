971b163374459feaf7a8ab2328afebcfd12bab6b
isliusar <isliusar@liaisonedu.com>
2023-04-26 11:27:44 +0300

    fix undefined method `client_middleware' for Sidekiq:Module (NoMethodError) for newer version of sidekiq

--- lib/sidekiq_status.rb
+++ lib/sidekiq_status.rb
@@ -6,8 +6,11 @@ require "sidekiq_status/version"
 require "sidekiq_status/client_middleware"
 require "sidekiq_status/container"
 require "sidekiq_status/worker"
-Sidekiq.client_middleware do |chain|
-  chain.add SidekiqStatus::ClientMiddleware
+
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    chain.add SidekiqStatus::ClientMiddleware
+  end
 end
 
 require 'sidekiq_status/web' if defined?(Sidekiq::Web)
